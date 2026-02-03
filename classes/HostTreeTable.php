9eb09db4ebd90c7f12353b6d01075ff0ade369cd
edinhofjr <ediofassbinder9876@gmail.com>
2026-02-03 07:46:52 -0300

    removed column sortes from host tree table

--- classes/HostTreeTable.php
+++ classes/HostTreeTable.php
@@ -11,12 +11,11 @@ class HostTreeTable extends CTableInfo {
     {
         parent::__construct();
         $this->setHeader([
-            //make_sorting_header(_('Name'), 'name', $data['sort'], $data['sortorder'], ''),
-            make_sorting_header(_('Name'), 'name', 'name', 'DESC', ''),
+            (new CColHeader(_('Name'))),
             (new CColHeader(_('Interface'))),
             (new CColHeader(_('Availability'))),
             (new CColHeader(_('Tags'))),
-            make_sorting_header(_('Status'), 'status', 'name', 'DESC', ''),
+            (new CColHeader(_('Status'))),
             (new CColHeader(_('Latest data'))),
             (new CColHeader(_('Problems'))),
             (new CColHeader(_('Graphs'))),
